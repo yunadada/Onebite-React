304d0e93c560ab87ce30ea6fb4feb4413375276c
yunadada <ynson0216@naver.com>
2024-08-25 01:11:34 +0900

    modify section05

--- onebite-react/section05/src/App.jsx
+++ onebite-react/section05/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
---
+++ onebite-react/section05/src/components/Register.jsx
@@ -0,0 +1,56 @@
+import { useState, useRef } from "react";
+// 간단한 회원가입 폼
+// 1. 이름
+// 2. 생년월일
+// 3. 국적
+// 4. 자기소개
+
+const Register = () => {
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+
+  const onChange = (e) => {
+    setInput({
+      ...input, //스프레드 연산자를 통해 관련없는 값들은 그대로 유지되도록 만들어줘야 함.
+      [e.target.name]: e.target.value, //e.target.name에 저장되어 있는 값(=이벤트가 발생한 태그의 name 속성에 설정된 값)으로 프로퍼티의 키를 설정
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
+      </div>
+      <div>
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
+      </div>
+      <div>
+        <select name="country" value={input.country} onChange={onChange}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+      </div>
+      <div>
+        <textarea name="bio" value={input.bio} onChange={onChange} />
+      </div>
+    </div>
+  );
+};
+
+export default Register;
