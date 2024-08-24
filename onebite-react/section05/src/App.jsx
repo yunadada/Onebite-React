6eb127114ef542f3173ea7548db9cd271cd62607
yunadada <ynson0216@naver.com>
2024-08-24 21:49:00 +0900

    modify section05

--- onebite-react/section05/src/App.jsx
+++ onebite-react/section05/src/App.jsx
@@ -1,27 +1,12 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
 
-// 부모 컴포넌트
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
-
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <div>자식요소</div>
-        <Header />
-      </Button>
+      <Bulb />
+      <Counter />
     </>
   );
 }
