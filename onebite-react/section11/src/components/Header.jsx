066d0138b2e05a5521f7bff815664ee5ef2dedc2
yunadada <ynson0216@naver.com>
2024-09-04 19:28:21 +0900

    add section11

---
+++ onebite-react/section11/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+import { memo } from "react";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h3>오늘은 📆</h3>
+      <h1>{new Date().toDateString()}</h1>
+    </div>
+  );
+};
+
+export default memo(Header);
+// 최적화가 이루어진 Header 컴포넌트를 반환
---
+++ onebite-react/section11/src/components/List.jsx
@@ -0,0 +1,73 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState, useMemo, useCallback, useContext } from "react";
+import { TodoContext } from "../App";
+
+const List = () => {
+  const { todos } = useContext(TodoContext);
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredDate = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredDate();
+
+  // const getAnalyzedData = () => {
+  //   const totalCount = todos.length;
+  //   const doneCount = todos.filter((todo) => todo.isDone).length;
+  //   const notDoneCount = totalCount - doneCount;
+
+  //   return {
+  //     totalCount,
+  //     doneCount,
+  //     notDoneCount,
+  //   };
+  // };
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
